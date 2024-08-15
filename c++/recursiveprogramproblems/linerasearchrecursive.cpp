0c4782faf42093a2158f5cd62ec1fbea727898ef
vscode <vscode.in>
2024-08-15 22:43:05 +0530

    15Augpart4

---
+++ c++/recursiveprogramproblems/linerasearchrecursive.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+bool linearsearch(int arr[],int x,int index,int n)
+{
+    if(index==n)
+    return 0;
+    if(arr[index]==x)
+    return 1;
+    return linearsearch(arr,x,index+1,n);
+}
+int main()
+{
+    int arr[]={3,6,2,9,7,5,9};
+    int x=9;
+    cout<<linearsearch(arr,x,0,7)<<endl;
+    return 0;
+}
