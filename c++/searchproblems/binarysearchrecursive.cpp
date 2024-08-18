d9bff514226515253edc0d53fe6d457246bbab65
vscode <shubhamdavid4606@gmail.com>
2024-08-18 22:00:12 +0530

    18Augpart2

---
+++ c++/searchproblems/binarysearchrecursive.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+bool binarysearch(int arr[],int start,int end,int x)
+{
+    if(start>end)
+    return 0;
+    int mid=start+(end-start)/2;
+    if(arr[mid]==x)
+    return 1;
+    else if(arr[mid]<x)
+    return binarysearch(arr,mid+1,end,x);
+    else 
+    return binarysearch(arr,start,end-1,x);
+}
+int main()
+{
+    int arr[]={2,4,6,8,10};
+    int x=10;
+    cout<<binarysearch(arr,0,5,x);
+    return 0;
+}
