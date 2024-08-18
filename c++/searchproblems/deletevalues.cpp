be69b284d2da3f3570247a409f01aac7596abc2d
vscode <vscode.in>
2024-08-18 20:30:29 +0530

    18Aug

---
+++ c++/searchproblems/deletevalues.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+vector<int >vnew;
+vnew.push_back(4);
+vnew.push_back(14);
+vnew.push_back(41);
+vnew.push_back(65);
+vnew.push_back(84);
+vnew.push_back(45);
+vnew.pop_back();
+cout<<"size of vnew:"<<vnew.size()<<endl;
+cout<<"capacity of vnew:"<<vnew.capacity()<<endl;
+}
