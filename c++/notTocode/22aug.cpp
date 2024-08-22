9836e79859f7bf51b6278fe400571c3ddad8cff8
vscode <shubhamdavid4606@gmail.com>
2024-08-22 23:29:54 +0530

    22aug

---
+++ c++/notTocode/22aug.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"today I am not in mode to code because of some reasons"<<endl;
+    cout<<"so don't mind it "<<endl;
+    cout<<"from tomorrow i will be back"<<endl;
+    return 0;
+}
