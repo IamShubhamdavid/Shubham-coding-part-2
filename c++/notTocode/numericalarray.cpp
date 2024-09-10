090cb838031590f91112595c9953c40851775d49
vscode <shubhamdavid4606@gmail.com>
2024-09-10 14:58:00 +0530

    10Septpart2

--- c++/notTocode/numericalarray.cpp
+++ c++/notTocode/numericalarray.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int idx,size;
-    int array[]={1,2,3,4,5};
+    int array[]={1,2,3,4,5,6};
     cout<<sizeof(array)<<endl;
     cout<<sizeof(array)/sizeof(array[0]);
     for( idx=0;idx<size;idx++)
