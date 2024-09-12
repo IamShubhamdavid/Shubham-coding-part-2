9436cd240e3fdfb7119a81655e2d9bd81ad7b2e8
vscode <shubhamdavid4606@gmail.com>
2024-09-12 13:50:23 +0530

    12Septpart4

--- c++/notTocode/CowAggresive.cpp
+++ c++/notTocode/CowAggresive.cpp
@@ -1,29 +1,3 @@
-/*#include<iostream>
-using namespace std;
-int stalls[],n,k,start;
-int end=stalls[n-1]-stalls[0];
-while(start<=end)
-{
-    int mid=start+(end-start)/2;
-    int count=1;
-    int pos=stalls[0];
-    for(int i=1;i<n;i++)
-    {
-        if(pos+mid<=stalls[i])
-        {
-            count++;
-            pos=stalls[i];
-        }
-        if(count<k)
-        end=mid-1;
-        else
-        {
-            ans=mid;
-            start=mid+1;
-        }
-    }
-    return 0;
-}*/
 #include<iostream>
 #include<algorithm> // For sorting the stalls array
 using namespace std;
