51526776f9981243c724845d09fd33229d196724
vscode <shubhamdavid4606@gmail.com>
2024-09-20 21:22:24 +0530

    20Septpart1

---
+++ c++/notTocode/continue.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //int num;
+    //cin>>num;
+    int i;
+    cin>>i;
+    while(i<5){
+        if(i==3)
+        continue; 
+        cout<<i<<endl;
+    }
+    return 0;
+}
