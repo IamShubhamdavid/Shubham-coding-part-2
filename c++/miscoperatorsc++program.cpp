d58fca73aa6ce36a69cdb73d06059dbfc4abf1cc
vscode <shubhamdavid4606@gmail.com>
2024-09-23 15:06:37 +0530

    23Septpart1

--- c++/miscoperatorsc++program.cpp
+++ c++/miscoperatorsc++program.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 int main(){
     int a=4;
-    cout<<size of (a)<<endl;
+    cout<<sizeof (a)<<endl;
     char name ='a';
-    cout<<size of (name)<<endl;
+    cout<<sizeof (name)<<endl;
     bool flag;
-    a==name?flag=true;flag=false;
+    a==name?
+    flag=true,flag=false;
     cout<<(&a)<<endl;
     return 0;
 
---
+++ c++/notTocode/multipleof5&7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    cin>>i;
+     if(i%7==0 && i%5==0)
+        cout<<i<<endl;
+        else(i%7!=0 && i%5!=0);
+        return -1;
+        //i+=5;
+    return 0;
+}
